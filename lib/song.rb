0693fc28ed192bdf1cb8cb1feb5d4d413bce5a15
Phil Billingsby <paulbillingsby@gmail.com>
2019-10-17 12:57:35 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -9,4 +9,14 @@ class Song
   def self.count
     @@count
   end
+  
+  def self.genres
+    count = 0
+    arr = []
+    genres.each do |n|
+      if n[count] === n[count + 1]
+        arr << n[count]
+        count += 1
+      end
+    end
 end
