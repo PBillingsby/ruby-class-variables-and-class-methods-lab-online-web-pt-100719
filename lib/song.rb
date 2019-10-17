a9d5a4469f54298cbc86ba1c22b5b8da050dfd33
Phil Billingsby <paulbillingsby@gmail.com>
2019-10-17 12:59:50 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -17,11 +17,5 @@ class Song
   def self.genres
     count = 0
     arr = []
-    genres.each do |n|
-      if n[count] === n[count + 1]
-        arr << n[count]
-        count += 1
-      end
-    end
   end
 end
